69b489d2532cb2b4db9b068a8a33d1a6cec7e9b0
nitred <nitish.k.reddy@gmail.com>
2018-09-05 18:35:43 +0200

    bugfix undid the changes that lead to templates going wrong

--- nr_pypackage/templates/package_flask_blueprints/PACKAGE_NAME_SAFE/service/server.py
+++ nr_pypackage/templates/package_flask_blueprints/PACKAGE_NAME_SAFE/service/server.py
@@ -1,10 +1,10 @@
 """Service."""
 from flask import Flask, redirect, url_for
 from nr_common.configreader import read_config
-{ % if blueprints['auth']['include'] % }
+{% if blueprints['auth']['include'] %}
 from flask_login import login_required
 from flask_session import Session
-{ % endif % }
+{% endif %}
 
 
 def create_app_without_blueprints(config):
@@ -32,33 +32,33 @@ def register_blueprints(app, config):
     from {{package_name_safe}}.service.blueprints.landing import landing_handler
     app.register_blueprint(landing_handler, url_prefix="/landing")
 
-    { % for blueprint_name, blueprint_kwargs in blueprints.items() % }
-    { % if blueprint_kwargs['include'] % }
+    {% for blueprint_name, blueprint_kwargs in blueprints.items() %}
+    {% if blueprint_kwargs['include'] %}
     from {{package_name_safe}}.service.blueprints.{{blueprint_name}} import {{blueprint_name}}_handler
     app.register_blueprint({{blueprint_name}}_handler, url_prefix="/{{ blueprint_name }}")
 
-    { % endif % }
-    { % endfor % }
+    {% endif %}
+    {% endfor %}
 
 
 def register_extensions(app, config):
     """Register extensions."""
-    { % if blueprints['auth']['include'] % }
+    {% if blueprints['auth']['include'] %}
     from {{package_name_safe}}.service.blueprints.auth import auth_manager
     auth_manager.init_app(app)
 
     sess = Session()
     sess.init_app(app)
-    { % endif % }
+    {% endif %}
 
 
 def register_routes(app, config):
     """Register routes to landing page."""
     @app.route("/")
-    { % if blueprints['auth']['include'] % }
+    {% if blueprints['auth']['include'] %}
 
     @login_required
-    { % endif % }
+    {% endif %}
 
     def index():
         return redirect(url_for('landing.index'))
